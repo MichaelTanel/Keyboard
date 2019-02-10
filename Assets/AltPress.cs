e06c867ba6d5f0697e575c411d0382d087091393
michael-10 <mtanel@live.ca>
2019-02-10 16:40:45 -0500

    Can press spacebar and alt buttons

---
+++ Assets/AltPress.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltPress : MonoBehaviour
+{
+
+    float m_distanceTraveled = 0f;
+    public bool keydown = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            Vector3 oldPosition = transform.position;
+            transform.Translate(0, 2 * Time.deltaTime, 0);
+            m_distanceTraveled += Vector3.Distance(oldPosition, transform.position);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            Vector3 oldPosition = transform.position;
+            transform.Translate(0, -2 * Time.deltaTime, 0);
+            m_distanceTraveled += Vector3.Distance(oldPosition, transform.position);
+        }
+    }
+
+}
---
+++ Assets/SpacePress.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacePress : MonoBehaviour
+{
+    public float clockwise = 1000.0f;
+
+    public float speed = 1f;
+    float m_distanceTraveled = 0f;
+    public bool keydown = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Vector3 oldPosition = transform.position;
+            transform.Translate(0, 2 * Time.deltaTime, 0);
+            m_distanceTraveled += Vector3.Distance(oldPosition, transform.position);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 oldPosition = transform.position;
+            transform.Translate(0, -2 * Time.deltaTime, 0);
+            m_distanceTraveled += Vector3.Distance(oldPosition, transform.position);
+            /*            if (!keydown) {
+                            keydown = true;
+                            Vector3 oldPosition = transform.position;
+                            transform.Translate(0, -2 * Time.deltaTime, 0);
+                            m_distanceTraveled += Vector3.Distance(oldPosition, transform.position);
+                        } else {
+                            keydown = false;
+
+                        }*/
+        }
+    }
+
+}
