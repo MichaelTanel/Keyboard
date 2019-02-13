68146febe1668990a482f8accba1a363c29a1671
michael-10 <mtanel@live.ca>
2019-02-13 17:44:15 -0500

    Added ability to toggle keyboard and cube

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonHandler : MonoBehaviour
+{
+
+    private bool state = false;
+    GameObject keyboard;
+    GameObject keyboard2;
+
+    public void Start()
+    {
+        keyboard = GameObject.Find("keyboard");
+        keyboard2 = GameObject.Find("Cube");
+        keyboard2.SetActive(false);
+    }
+
+    public void SetText(string text)
+    {
+        keyboard.SetActive(state);
+        keyboard2.SetActive(!state);
+        state = !state;
+    }
+
+
+}
---
+++ Assets/EntireKeyboard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntireKeyboard : MonoBehaviour
+{
+    float m_distanceTraveled = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            gameObject.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+}
